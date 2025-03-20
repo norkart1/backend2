6e994fccd4d194da75f45986b6697400ed4cacf9
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-20 11:33:29 +0000

    Hh

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get("/register", async (req, res) => {
+    res.send("register");
+});
+
+router.get("/login", async (req, res) => {
+    res.send("login");
+});
+
+
+export default router;
